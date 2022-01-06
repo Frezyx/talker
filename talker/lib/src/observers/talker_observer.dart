63376793f61f68670022c0b82c5b804a427f0475
Stanislav Ilin <youngfrezyx@gmail.com>
2022-01-06 15:15:53 +0300

    Make observer real class

--- talker/lib/src/observers/talker_observer.dart
+++ talker/lib/src/observers/talker_observer.dart
@@ -1,8 +1,12 @@
 import 'package:talker/src/models/talker_data/talker_data.dart';
 import 'package:talker_error_handler/talker_error_handler.dart';
 
-//TODO: make it real class
-abstract class TalkerObserver {
-  Function(ErrorDetails err) get onError;
-  Function(TalkerDataInterface data) get onLog;
+class TalkerObserver {
+  const TalkerObserver({
+    required this.onError,
+    required this.onLog,
+  });
+
+  final Function(ErrorDetails err) onError;
+  final Function(TalkerDataInterface data) onLog;
 }

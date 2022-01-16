448688c9442ea3650d497410d2a7daeedf8a26ed
Stanislav Ilin <youngfrezyx@gmail.com>
2022-01-16 19:04:52 +0500

    Add docs to TalkerLog

--- talker/lib/src/models/talker_data/talker_log.dart
+++ talker/lib/src/models/talker_data/talker_log.dart
@@ -1,5 +1,7 @@
 import 'package:talker/talker.dart';
 
+/// Base implementation of [TalkerDataInterface]
+/// to create Logs
 class TalkerLog implements TalkerDataInterface {
   TalkerLog(
     this.message, {
@@ -14,33 +16,40 @@ class TalkerLog implements TalkerDataInterface {
     _time = time ?? DateTime.now();
   }
 
+  late DateTime _time;
+
+  /// {@macro talker_data_message}
   @override
   final String message;
 
   @override
   final Exception? exception;
 
+  /// {@macro talker_data_error}
   @override
   final Error? error;
 
+  /// {@macro talker_data_stackTrace}
   @override
   final StackTrace? stackTrace;
 
   @override
   final Map<String, dynamic>? additional;
 
+  /// {@macro talker_data_loglevel}
   @override
   final LogLevel? logLevel;
 
+  /// {@macro talker_data_generateTextMessage}
   @override
   String generateTextMessage() {
     return '$displayTitle$message$displayAditional';
   }
 
+  /// {@macro talker_data_time}
   @override
   DateTime get time => _time;
 
-  late DateTime _time;
-
+  /// [AnsiPen?] [pen] - sets your own log color for console
   final AnsiPen? pen;
 }

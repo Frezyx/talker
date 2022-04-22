6ef382bfcaca0556291c9a61ce10ffd5867c8420
Stanislav Ilin <youngfrezyx@gmail.com>
2022-04-22 18:48:48 +0500

    Delete intl package

--- packages/talker/lib/src/utils/formaters/date_time_formater.dart
+++ packages/talker/lib/src/utils/formaters/date_time_formater.dart
@@ -1,5 +1,3 @@
-import 'package:intl/intl.dart';
-
 /// Util for [DateTime] formating
 class TalkerDateTimeFormater {
   const TalkerDateTimeFormater(this.date);
@@ -10,6 +8,7 @@ class TalkerDateTimeFormater {
   /// Get time and seconds for display in UI
   /// Format ['HH:mm:s ms']
   String get timeAndSeconds {
-    return '${DateFormat('HH:mm:s ms').format(date)}ms';
+    final d = date;
+    return '${d.hour}:${d.minute}:${d.second} ${d.millisecond}ms';
   }
 }

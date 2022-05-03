035b9b91b89a37dce5d46ecb7699c02d8a4622dd
Stanislav Ilin <youngfrezyx@gmail.com>
2022-05-03 20:39:13 +0500

    Implement bash scripts to pub_get_all

---
+++ utils/pub_get_all_offline.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "talker"
+cd packages/talker
+flutter pub get --offline
+
+echo "talker_flutter"
+cd ../talker_flutter
+flutter pub get --offline
+
+echo "talker_logger"
+cd ../talker_logger
+flutter pub get --offline

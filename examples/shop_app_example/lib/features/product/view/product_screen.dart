bb99cc4de0bb128e254bf4a692ab7f6d16f18400
Stanislav Ilin <youngfrezyx@gmail.com>
2022-05-28 20:04:44 +0500

    Implement product app UI

--- examples/shop_app_example/lib/features/product/view/product_screen.dart
+++ examples/shop_app_example/lib/features/product/view/product_screen.dart
@@ -39,13 +39,158 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       body: BlocBuilder<ProductBloc, ProductState>(
         bloc: _productBloc,
         builder: (context, state) {
           if (state is ProductLoaded) {
-            return Column(
-              children: [],
+            final product = state.product;
+            return Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      stops: [-0.3, 0.9],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFFF1F3F4),
+                        Color(0xFFE3E4E4),
+                      ],
+                    ),
+                  ),
+                ),
+                Image.asset(
+                  product.image,
+                  width: double.infinity,
+                ),
+              ],
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+      bottomNavigationBar: BlocBuilder<ProductBloc, ProductState>(
+        bloc: _productBloc,
+        builder: (context, state) {
+          if (state is ProductLoaded) {
+            final product = state.product;
+            return Container(
+              height: 500,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: theme.cardColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[400]!,
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            product.name,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Text(
+                            product.type,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Text(
+                            '${product.price} \$',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(flex: 1),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Flexible(
+                          child: Text(
+                            'Product details',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Text(
+                            '''It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ''',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(flex: 2),
+                    SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FloatingActionButton(
+                              backgroundColor: theme.primaryColor,
+                              child: const Icon(
+                                Icons.favorite,
+                              ),
+                              onPressed: () {},
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: FloatingActionButton.extended(
+                                backgroundColor: theme.primaryColor,
+                                onPressed: () {},
+                                label: Row(
+                                  children: const [
+                                    Icon(Icons.shopping_basket),
+                                    SizedBox(width: 10),
+                                    Text('Add to card'),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             );
           }
           if (state is ProductLoadingFailure) {

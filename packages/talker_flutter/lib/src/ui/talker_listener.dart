581c41b40ea97a03ebdbddf11b68bc0f6392fe0c
Stanislav Ilin <youngfrezyx@gmail.com>
2022-06-25 20:06:37 +0500

    Update docs

--- packages/talker_flutter/lib/src/ui/talker_listener.dart
+++ packages/talker_flutter/lib/src/ui/talker_listener.dart
@@ -3,6 +3,29 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
+/// It should be used for app functionality like
+/// showing [SnackBar] or [Dialog] about error or exception
+///
+/// The [listener] be called when [Talker.stream] recive new message
+///
+///```dart
+///TalkerListener(
+///  talker: talker,
+///  listener: (data) => _talkerListener(context, data),
+///  child: Text('Your app or app screen'),
+///),
+///
+/// void _talkerListener(BuildContext context, TalkerDataInterface data) {
+///  if (data is TalkerException || data is TalkerError) {
+///    ScaffoldMessenger.of(context).showSnackBar(
+///      SnackBar(
+///        content: Text(data.displayMessage),
+///      ),
+///    );
+///  }
+///}
+///```
+///
 class TalkerListener extends StatefulWidget {
   const TalkerListener({
     Key? key,

5b61ddd7c3b7a250197ed3c5f2dad53014fbb0b1
Stanislav Ilin <youngfrezyx@gmail.com>
2022-08-07 11:08:02 +0500

    Decompose widgets

--- packages/talker_flutter/example/lib/custom_error_messages_example/custom_error_messages_example.dart
+++ packages/talker_flutter/example/lib/custom_error_messages_example/custom_error_messages_example.dart
@@ -60,55 +60,7 @@ class _Home extends StatelessWidget {
         behavior: SnackBarBehavior.floating,
         backgroundColor: Colors.transparent,
         elevation: 0,
-        content: Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Colors.red,
-            boxShadow: const [
-              BoxShadow(
-                color: Color(0x19000000),
-                spreadRadius: 2,
-                blurRadius: 10,
-                offset: Offset(2, 4),
-              )
-            ],
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Icon(Icons.warning, color: Colors.white),
-              const Spacer(),
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Oh no !',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    data.exception.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              const Spacer(),
-              TextButton(
-                onPressed: () =>
-                    ScaffoldMessenger.of(context).hideCurrentSnackBar(),
-                child: const Text("Undo"),
-              )
-            ],
-          ),
-        ),
+        content: ExceptionSnackbar(data: data),
       ),
     );
   }
@@ -127,5 +79,68 @@ class _Home extends StatelessWidget {
     } catch (e, st) {
       talker.handle(e, st);
     }
+  }
+}
+
+class ExceptionSnackbar extends StatelessWidget {
+  const ExceptionSnackbar({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final TalkerException data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.red,
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x19000000),
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: Offset(2, 4),
+          )
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(width: 10),
+          const Icon(Icons.warning, color: Colors.white),
+          const Spacer(),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Oh no !',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                data.exception.toString(),
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          TextButton(
+            onPressed: () =>
+                ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+            child: const Text("Undo"),
+          )
+        ],
+      ),
+    );
   }
 }

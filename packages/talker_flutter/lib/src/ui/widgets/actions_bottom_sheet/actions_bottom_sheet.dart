d95cbde4e73aeaf989a4fac7040dde912311d46e
Stanislav Ilin <youngfrezyx@gmail.com>
2022-09-28 22:19:36 +0500

    Fix paddings

--- packages/talker_flutter/lib/src/ui/widgets/actions_bottom_sheet/actions_bottom_sheet.dart
+++ packages/talker_flutter/lib/src/ui/widgets/actions_bottom_sheet/actions_bottom_sheet.dart
@@ -15,6 +15,10 @@ class ActionsBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(
+        top: 20,
+        bottom: MediaQuery.of(context).padding.bottom,
+      ),
       decoration: BoxDecoration(
         color: talkerScreenTheme.backgroudColor,
         borderRadius: BorderRadius.circular(10),
@@ -22,7 +26,6 @@ class ActionsBottomSheet extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const SizedBox(height: 20),
           ...actions
               .map(
                 (e) => _ActionTile(

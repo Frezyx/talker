79a444af1013fef0f81d9a55b894b54be9c9dad3
Stanislav Ilin <youngfrezyx@gmail.com>
2022-10-02 01:43:58 +0600

    Fix empty data and headers prints

--- packages/talker_dio_logger/lib/http_logs.dart
+++ packages/talker_dio_logger/lib/http_logs.dart
@@ -36,11 +36,11 @@ class HttpRequestLog extends FlutterTalkerLog {
   String generateTextMessage() {
     var msg = '[$displayTitle] $message';
 
-    if (printData) {
+    if (printData && data != null) {
       final prettyData = encoder.convert(data);
       msg += '\nDATA:$prettyData';
     }
-    if (printHeaders) {
+    if (printHeaders && headers.isNotEmpty) {
       final prettyHeaders = encoder.convert(headers);
       msg += '\nHEADERS:$prettyHeaders';
     }

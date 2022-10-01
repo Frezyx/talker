40fa047d4b7fbfeccec039bf6d2aa3f1ef1e2d36
Stanislav Ilin <youngfrezyx@gmail.com>
2022-10-02 00:20:28 +0600

    Init dio logger package

---
+++ packages/talker_dio_logger/example/talker_dio_logger_example.dart
@@ -0,0 +1,6 @@
+import 'package:talker_dio_logger/talker_dio_logger.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/talker_dio_logger/lib/talker_dio_logger.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library talker_dio_logger;
+
+export 'src/talker_dio_logger_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.

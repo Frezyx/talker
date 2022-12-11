5461bf57ee8f17519e8d2029842683cf2714f79b
Stanislav Ilin <youngfrezyx@gmail.com>
2022-12-11 14:18:29 +0600

    Upgrade TalkerMonitor UI

--- packages/talker_flutter/lib/src/ui/talker_monitor/talker_monitor.dart
+++ packages/talker_flutter/lib/src/ui/talker_monitor/talker_monitor.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:talker_dio_logger/http_logs.dart';
+import 'package:talker_flutter/src/ui/talker_monitor/talker_monitor_item.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 class TalkerMonitor extends StatelessWidget {
@@ -31,102 +32,89 @@ class TalkerMonitor extends StatelessWidget {
           final errors = data.whereType<TalkerError>().toList();
           final exceptions = data.whereType<TalkerException>().toList();
           final httpRequests = data.whereType<HttpRequestLog>().toList();
+          final httpErrors = data.whereType<HttpErrorLog>().toList();
+          final httpResponses = data.whereType<HttpResponseLog>().toList();
 
           return CustomScrollView(
             slivers: [
-              const SliverToBoxAdapter(child: SizedBox(height: 10)),
-              SliverToBoxAdapter(
-                child: TalkerMonitorItem(
-                  logs: errors,
-                  title: 'Errors',
-                  color: Colors.red,
-                  icon: Icons.error_outline_rounded,
-                  subtitle:
-                      'Application has ${errors.length} unresolved errors',
+              if (errors.isNotEmpty) ...[
+                const SliverToBoxAdapter(child: SizedBox(height: 10)),
+                SliverToBoxAdapter(
+                  child: TalkerMonitorItem(
+                    logs: errors,
+                    title: 'Errors',
+                    color: Colors.red,
+                    icon: Icons.error_outline_rounded,
+                    subtitle:
+                        'Application has ${errors.length} unresolved errors',
+                  ),
                 ),
-              ),
-              const SliverToBoxAdapter(child: SizedBox(height: 10)),
-              SliverToBoxAdapter(
-                child: TalkerMonitorItem(
-                  logs: exceptions,
-                  title: 'Exceptions',
-                  color: Colors.red,
-                  icon: Icons.error_outline_rounded,
-                  subtitle:
-                      'Application has ${exceptions.length} unresolved exceptions',
+              ],
+              if (exceptions.isNotEmpty) ...[
+                const SliverToBoxAdapter(child: SizedBox(height: 10)),
+                SliverToBoxAdapter(
+                  child: TalkerMonitorItem(
+                    logs: exceptions,
+                    title: 'Exceptions',
+                    color: Colors.red,
+                    icon: Icons.error_outline_rounded,
+                    subtitle:
+                        'Application has ${exceptions.length} unresolved exceptions',
+                  ),
                 ),
-              ),
-              const SliverToBoxAdapter(child: SizedBox(height: 10)),
-              SliverToBoxAdapter(
-                child: TalkerMonitorItem(
-                  logs: httpRequests,
-                  title: 'Http Requests',
-                  color: Colors.green,
-                  icon: Icons.wifi,
-                  subtitle: '${httpRequests.length} http requests executed',
+              ],
+              if (httpRequests.isNotEmpty) ...[
+                const SliverToBoxAdapter(child: SizedBox(height: 10)),
+                SliverToBoxAdapter(
+                  child: TalkerMonitorItem(
+                    logs: httpRequests,
+                    title: 'Http Requests',
+                    color: Colors.green,
+                    icon: Icons.wifi,
+                    subtitleWidget: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: '${httpRequests.length}',
+                            style: const TextStyle(color: Colors.white),
+                            children: const [
+                              TextSpan(text: ' http requests executed')
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: '${httpResponses.length} successful',
+                            style: const TextStyle(color: Colors.green),
+                            children: const [
+                              TextSpan(
+                                text: ' responses recived',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: '${httpErrors.length} failure',
+                            style: const TextStyle(color: Colors.red),
+                            children: const [
+                              TextSpan(
+                                text: ' responses recived',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              )
+              ],
             ],
           );
         },
-      ),
-    );
-  }
-}
-
-class TalkerMonitorItem extends StatelessWidget {
-  const TalkerMonitorItem({
-    Key? key,
-    required this.logs,
-    required this.title,
-    this.subtitle,
-    required this.color,
-    required this.icon,
-  }) : super(key: key);
-
-  final String title;
-  final String? subtitle;
-  final List<TalkerDataInterface> logs;
-  final Color color;
-  final IconData icon;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(8),
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 49, 49, 49),
-        border: Border.all(color: color),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Icon(icon, color: color),
-          const SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  color: color,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              if (subtitle != null)
-                Text(
-                  subtitle!,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                  ),
-                ),
-            ],
-          ),
-        ],
       ),
     );
   }
---
+++ packages/talker_flutter/lib/src/ui/talker_monitor/talker_monitor_item.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+class TalkerMonitorItem extends StatelessWidget {
+  const TalkerMonitorItem({
+    Key? key,
+    required this.logs,
+    required this.title,
+    this.subtitle,
+    this.subtitleWidget,
+    required this.color,
+    required this.icon,
+  }) : super(key: key);
+
+  final String title;
+  final String? subtitle;
+  final Widget? subtitleWidget;
+  final List<TalkerDataInterface> logs;
+  final Color color;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 49, 49, 49),
+        border: Border.all(color: color),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(icon, color: color),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: color,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  if (subtitle != null)
+                    Text(
+                      subtitle!,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  if (subtitleWidget != null) subtitleWidget!
+                ],
+              ),
+            ],
+          ),
+          Icon(Icons.arrow_forward_ios_rounded, color: color),
+        ],
+      ),
+    );
+  }
+}

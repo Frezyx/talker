53bebc3ce21c3ee4520ff49b8ed6618bbd284a00
Stanislav Ilin <youngfrezyx@gmail.com>
2022-12-31 19:59:09 +0500

    Add mustCallSuper

--- packages/talker_bloc_logger/lib/talker_bloc_logger_observer.dart
+++ packages/talker_bloc_logger/lib/talker_bloc_logger_observer.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+// ignore: depend_on_referenced_packages
+import 'package:meta/meta.dart';
 import 'package:talker/talker.dart';
 import 'package:talker_bloc_logger/bloc_logs.dart';
 
@@ -12,18 +14,21 @@ class TalkerBlocObserver extends BlocObserver {
   late TalkerInterface _talker;
 
   @override
+  @mustCallSuper
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
     _talker.logTyped(BlocEventLog(bloc, event));
   }
 
   @override
+  @mustCallSuper
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
     _talker.logTyped(BlocStateLog(bloc, transition));
   }
 
   @override
+  @mustCallSuper
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
     _talker.error('${bloc.runtimeType}', error, stackTrace);

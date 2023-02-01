2c8626b49dcd3d0eb9ef585d5a59dadf87cb17a3
Stanislav Ilin <youngfrezyx@gmail.com>
2023-02-01 20:16:12 +0500

    Add equality operators override for talker and settings

--- packages/talker/lib/src/talker.dart
+++ packages/talker/lib/src/talker.dart
@@ -375,4 +375,23 @@ class Talker implements TalkerInterface {
     final approved = _filter?.filter(data) ?? true;
     return approved;
   }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Talker &&
+        other.settings == settings &&
+        other._logger == _logger &&
+        other._errorHandler == _errorHandler &&
+        other._filter == _filter;
+  }
+
+  @override
+  int get hashCode {
+    return settings.hashCode ^
+        _logger.hashCode ^
+        _errorHandler.hashCode ^
+        _filter.hashCode;
+  }
 }

264363020f6116b92d383035d41836878976b73d
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-02-24 00:07:39 +0200

    Using the `join` function from the `path` package is best practice to ensure the path is correctly constructed for each platform.

--- packages/talker_flutter/lib/src/controller/talker_screen_controller.dart
+++ packages/talker_flutter/lib/src/controller/talker_screen_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 

a20389c9b3c77fdf507752ce7dc0a84ec58e0a36
Stanislav Ilin <youngfrezyx@gmail.com>
2023-05-18 21:10:12 +0400

    Add analytics example for talker example

--- packages/talker/example/talker_example.dart
+++ packages/talker/example/talker_example.dart
@@ -22,12 +22,13 @@ Future<void> main() async {
   talker.warning('Log warning');
   talker.critical('Log critical');
 
-  final httpLog = HttpTalkerLog('Http good');
+  final httpLog = HttpExampleTalkerLog('Http good');
   talker.logTyped(httpLog);
+  talker.logTyped(AnalyticsExampleTalkerLog(talker));
 }
 
-class HttpTalkerLog extends TalkerLog {
-  HttpTalkerLog(String message) : super(message);
+class HttpExampleTalkerLog extends TalkerLog {
+  HttpExampleTalkerLog(String message) : super(message);
 
   @override
   AnsiPen get pen => AnsiPen()..xterm(49);
@@ -35,5 +36,26 @@ class HttpTalkerLog extends TalkerLog {
   @override
   String generateTextMessage() {
     return pen.write(message);
+  }
+}
+
+class AnalyticsExampleTalkerLog extends TalkerLog {
+  AnalyticsExampleTalkerLog(this.talker) : super('Report');
+
+  final Talker talker;
+
+  @override
+  String get title => 'ANALYTICS';
+
+  @override
+  AnsiPen get pen => AnsiPen()..xterm(121);
+
+  @override
+  String generateTextMessage() {
+    final msg = '''
+$displayTitleWithTime$message
+Registred logs: ${talker.history.length}
+Registred symbols: ${talker.history.join().split('').length}''';
+    return msg;
   }
 }

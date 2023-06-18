98243775797f64eb4e9a49af3bc2d5d2ba537747
Stanislav Ilin <youngfrezyx@gmail.com>
2023-06-18 12:34:25 +0500

    Rename http_logs.dart to dio_logs.dart

--- packages/talker_dio_logger/lib/talker_dio_logger_interceptor.dart
+++ packages/talker_dio_logger/lib/talker_dio_logger_interceptor.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:talker/talker.dart';
-import 'package:talker_dio_logger/http_logs.dart';
+import 'package:talker_dio_logger/dio_logs.dart';
 import 'package:talker_dio_logger/talker_dio_logger.dart';
 
 /// [Dio] http client logger on [Talker] base

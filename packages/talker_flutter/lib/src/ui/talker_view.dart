1e6a4ef54a52dd6e75e43df3c1cd34ac2272b3d8
Stanislav Ilin <youngfrezyx@gmail.com>
2023-08-05 22:36:40 +0500

    Extract TalkerViewAppBar widget

--- packages/talker_flutter/lib/src/ui/talker_view.dart
+++ packages/talker_flutter/lib/src/ui/talker_view.dart
@@ -45,7 +45,6 @@ class _TalkerViewState extends State<TalkerView> {
   @override
   Widget build(BuildContext context) {
     final talkerTheme = widget.theme;
-    final theme = Theme.of(context);
     return AnimatedBuilder(
       animation: widget.controller,
       builder: (context, child) {
@@ -59,109 +58,19 @@ class _TalkerViewState extends State<TalkerView> {
             return CustomScrollView(
               physics: const BouncingScrollPhysics(),
               slivers: [
-                SliverAppBar(
-                  backgroundColor: talkerTheme.backgroundColor,
-                  elevation: 0,
-                  pinned: true,
-                  floating: true,
-                  expandedHeight: 180,
-                  collapsedHeight: 60,
-                  toolbarHeight: 60,
-                  actions: [
-                    UnconstrainedBox(
-                      child: _MonitorButton(
-                        talker: widget.talker,
-                        onPressed: () => _openTalkerMonitor(context),
-                      ),
-                    ),
-                    UnconstrainedBox(
-                      child: IconButton(
-                        onPressed: () => _openTalkerSettings(
-                          context,
-                          talkerTheme,
-                        ),
-                        icon: const Icon(Icons.settings_rounded),
-                      ),
-                    ),
-                    UnconstrainedBox(
-                      child: IconButton(
-                        onPressed: () => _showActionsBottomSheet(context),
-                        icon: const Icon(Icons.menu_rounded),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                  ],
-                  title: Text(widget.appBarTitle),
-                  flexibleSpace: FlexibleSpaceBar(
-                    collapseMode: CollapseMode.parallax,
-                    background: SafeArea(
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 60),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 50,
-                              child: ListView(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                ),
-                                scrollDirection: Axis.horizontal,
-                                children: [
-                                  GroupButton(
-                                    controller: _titilesController,
-                                    isRadio: false,
-                                    buttonBuilder: (selected, value, context) {
-                                      final count = titles
-                                          .where((e) => e == value)
-                                          .length;
-                                      return Container(
-                                        padding: const EdgeInsets.all(8),
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          color: selected
-                                              ? theme.primaryColor
-                                              : cardBackgroundColor,
-                                        ),
-                                        child: Row(
-                                          children: [
-                                            Text(
-                                              '$count',
-                                              style: const TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.white,
-                                              ),
-                                            ),
-                                            const SizedBox(width: 4),
-                                            Text(
-                                              '$value',
-                                              style: const TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.white,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      );
-                                    },
-                                    onSelected: (_, i, selected) {
-                                      _onToggleTitle(unicTitles[i], selected);
-                                    },
-                                    buttons: unicTitles,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            _SearchTextField(
-                              controller: widget.controller,
-                              talkerScreenTheme: talkerTheme,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                TalkerViewAppBar(
+                  title: widget.appBarTitle,
+                  talker: widget.talker,
+                  talkerTheme: talkerTheme,
+                  titilesController: _titilesController,
+                  titles: titles,
+                  unicTitles: unicTitles,
+                  controller: widget.controller,
+                  onMonitorTap: () => _openTalkerMonitor(context),
+                  onActionsTap: () => _showActionsBottomSheet(context),
+                  onSettingsTap: () =>
+                      _openTalkerSettings(context, talkerTheme),
+                  onToggleTitle: _onToggleTitle,
                 ),
                 const SliverToBoxAdapter(child: SizedBox(height: 8)),
                 SliverList(
@@ -309,6 +218,141 @@ class _TalkerViewState extends State<TalkerView> {
   void _copyAllLogs(BuildContext context) {
     Clipboard.setData(ClipboardData(text: widget.talker.history.text));
     _showSnackBar(context, 'All logs copied in buffer');
+  }
+}
+
+class TalkerViewAppBar extends StatelessWidget {
+  const TalkerViewAppBar({
+    Key? key,
+    this.title,
+    required this.talker,
+    required this.talkerTheme,
+    required this.titilesController,
+    required this.controller,
+    required this.titles,
+    required this.unicTitles,
+    required this.onMonitorTap,
+    required this.onSettingsTap,
+    required this.onActionsTap,
+    required this.onToggleTitle,
+  }) : super(key: key);
+
+  final String? title;
+
+  final Talker talker;
+  final TalkerScreenTheme talkerTheme;
+  final GroupButtonController titilesController;
+  final TalkerScreenController controller;
+
+  final List<String> titles;
+  final List<String> unicTitles;
+
+  final VoidCallback onMonitorTap;
+  final VoidCallback onSettingsTap;
+  final VoidCallback onActionsTap;
+
+  final Function(String title, bool selected) onToggleTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SliverAppBar(
+      backgroundColor: talkerTheme.backgroundColor,
+      elevation: 0,
+      pinned: true,
+      floating: true,
+      expandedHeight: 180,
+      collapsedHeight: 60,
+      toolbarHeight: 60,
+      actions: [
+        UnconstrainedBox(
+          child: _MonitorButton(
+            talker: talker,
+            onPressed: onMonitorTap,
+          ),
+        ),
+        UnconstrainedBox(
+          child: IconButton(
+            onPressed: onSettingsTap,
+            icon: const Icon(Icons.settings_rounded),
+          ),
+        ),
+        UnconstrainedBox(
+          child: IconButton(
+            onPressed: onActionsTap,
+            icon: const Icon(Icons.menu_rounded),
+          ),
+        ),
+        const SizedBox(width: 10),
+      ],
+      title: title != null ? Text(title!) : null,
+      flexibleSpace: FlexibleSpaceBar(
+        collapseMode: CollapseMode.parallax,
+        background: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      GroupButton(
+                        controller: titilesController,
+                        isRadio: false,
+                        buttonBuilder: (selected, value, context) {
+                          final count = titles.where((e) => e == value).length;
+                          return Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: selected
+                                  ? theme.primaryColor
+                                  : cardBackgroundColor,
+                            ),
+                            child: Row(
+                              children: [
+                                Text(
+                                  '$count',
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(width: 4),
+                                Text(
+                                  '$value',
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        onSelected: (_, i, selected) =>
+                            onToggleTitle(unicTitles[i], selected),
+                        buttons: unicTitles,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 4),
+                _SearchTextField(
+                  controller: controller,
+                  talkerScreenTheme: talkerTheme,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 

a5c7644d572b1a1bea294f8ae08928db1a78541d
june lee <melodysdreamj@gmail.com>
2024-03-01 22:55:32 +0900

    Update logger.dart

--- packages/talker_logger/lib/src/logger.dart
+++ packages/talker_logger/lib/src/logger.dart
@@ -1,4 +1,4 @@
-import 'package:talker_logger/src/logger_io.dart' if (dart.library.html) 'logger_output_web.dart' as log_output;
+import 'package:talker_logger/src/logger_io.dart' if (dart.library.html) 'logger_web.dart' as log_output;
 import 'package:talker_logger/talker_logger.dart';
 
 class TalkerLogger {

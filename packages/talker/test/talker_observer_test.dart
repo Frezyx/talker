e044ef3932b605fab370c8bd1506e06d6b0012f4
Boros Máté <33902750+qwadrox@users.noreply.github.com>
2024-03-05 15:37:55 +0100

    test: :test_tube: Add tests for observer's log methods

--- packages/talker/test/talker_observer_test.dart
+++ packages/talker/test/talker_observer_test.dart
@@ -46,4 +46,122 @@ void main() {
       expect(_exceptionsStore, isEmpty);
     });
   });
+
+  group('Test observer\'s log method invoked with the correct log', () {
+    late Talker talker;
+    final mockObserver = MockTalkerObserver();
+
+    setUp(() {
+      talker = Talker(observer: mockObserver);
+      talker.cleanHistory();
+      _logsStore.clear();
+      _errorsStore.clear();
+      _exceptionsStore.clear();
+    });
+
+    test('info method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString('Test stack');
+      final logMessage = 'log';
+      talker.info(logMessage, exception, stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+
+    test('error method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString("Test stack");
+      final logMessage = 'log';
+      talker.error(logMessage, exception, stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+
+    test('verbose method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString("Test stack");
+      final logMessage = 'log';
+      talker.verbose(logMessage, exception, stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+
+    test('warning method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString("Test stack");
+      final logMessage = 'log';
+      talker.warning(logMessage, exception, stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+
+    test('debug method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString("Test stack");
+      final logMessage = 'log';
+      talker.debug(logMessage, exception, stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+
+    test('critical method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString("Test stack");
+      final logMessage = 'log';
+      talker.critical(logMessage, exception, stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+
+    test('custom log level method logs correct message, exception, and stack trace', () {
+      final exception = "Test exception";
+      final stack = StackTrace.fromString("Test stack");
+      final logMessage = 'log';
+      talker.log(logMessage, logLevel: LogLevel.debug, exception: exception, stackTrace: stack);
+
+      expect(_logsStore.last.message, equals(logMessage));
+      expect(_logsStore.last.exception.toString(), equals('Exception: $exception'));
+      expect(_logsStore.last.stackTrace, equals(stack));
+
+      expect(_logsStore, isNotEmpty);
+      expect(_errorsStore, isEmpty);
+      expect(_exceptionsStore, isEmpty);
+    });
+  });
 }

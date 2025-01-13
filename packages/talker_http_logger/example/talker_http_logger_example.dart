9cf957e3d439c3186c47fd21f221134edf4e1741
ilya <erasilka@yahoo.com>
2025-01-13 13:52:37 +0300

    Exclude a specific header from TalkerHttpLogger
    (cherry picked from commit 35f0d8a416c4387ec3fb8e7b9e99ba2f6cc2a7e3)

--- packages/talker_http_logger/example/talker_http_logger_example.dart
+++ packages/talker_http_logger/example/talker_http_logger_example.dart
@@ -1,10 +1,17 @@
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:talker_http_logger/talker_http_logger.dart';
+import 'package:talker_http_logger/talker_http_logger_settings.dart';
 
 void main(List<String> args) async {
   final client = InterceptedClient.build(interceptors: [
-    TalkerHttpLogger(),
+    TalkerHttpLogger(
+        settings: TalkerHttpLoggerSettings(
+            hideHeaderValuesForKeys: {'Authorization'})),
   ]);
 
-  await client.get("https://google.com".toUri());
+  await client.get("https://google.com".toUri(), headers: {
+    "firstHeader": "firstHeaderValue",
+    "authorization": "bearer super_secret_token",
+    "lastHeader": "lastHeaderValue",
+  });
 }

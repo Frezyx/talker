6c83c10e3ef4226d2256a95703004d195c1101d0
Yelaman Yelmuratov <yelamanyelmuratov@gmail.com>
2025-01-14 16:44:50 +0500

    @ F B correctly migrate to wasm support

--- packages/talker_flutter/lib/src/utils/download_logs/download_logs_web.dart
+++ packages/talker_flutter/lib/src/utils/download_logs/download_logs_web.dart
@@ -1,13 +1,17 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
+import 'dart:js_interop';
+
+import 'package:web/web.dart';
 
 Future<void> downloadFile(String logs) async {
-  final Blob blob = Blob(<String>[logs], 'text/plain', 'native');
-  final String fmtDate = DateTime.now().toString().replaceAll(':', ' ');
+  final jsArray = JSArray.from<JSString>(logs.toJS as JSObject);
+  final blob = Blob(jsArray, BlobPropertyBag(type: 'text/plain'));
 
-  AnchorElement(
-    href: Url.createObjectUrlFromBlob(blob),
-  )
-    ..setAttribute('download', 'talker_logs_$fmtDate.txt')
+  final fmtDate = DateTime.now().toString().replaceAll(':', ' ');
+
+  final anchor = document.createElement('a') as HTMLAnchorElement
+    ..href = URL.createObjectURL(blob)
+    ..download = 'talker_logs_$fmtDate.txt'
     ..click();
+
+  URL.revokeObjectURL(anchor.href);
 }

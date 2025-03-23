c1f8d4240abbefc7e0296ec84503814111bc5551
Mooyee <danielfagadau@gmail.com>
2025-03-23 14:07:07 +0100

    bugfix/web_encoding: fix web specific encoding issue

--- packages/talker_logger/lib/src/logger_web.dart
+++ packages/talker_logger/lib/src/logger_web.dart
@@ -2,5 +2,9 @@ import 'dart:js_interop';
 
 import 'package:web/web.dart';
 
-void outputLog(String message) =>
-    message.split('\n').forEach((it) => console.log(it.toJS));
+// Note: \uFFFD is a special character that can cause a crash on web when logged
+// Encountered while logging some italian stuff with accents (èéàùìò), not always happening
+// Probably related to enconding issues.
+void outputLog(String message) => message
+    .split('\n')
+    .forEach((it) => console.log(it.replaceAll('\uFFFD', '').toJS));

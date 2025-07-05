f019ad8d9a692c0914853489d611227c752bd9cf
Stanislav Ilin <youngfrezyx@gmail.com>
2025-07-05 20:02:57 +0500

    Replace abstract TalkerFilter with BaseTalkerFilter

--- packages/talker/lib/src/filter.dart
+++ packages/talker/lib/src/filter.dart
@@ -1,25 +1,10 @@
 import 'package:talker/talker.dart';
 
-typedef TalkerFilter = _Filter<TalkerData>;
-
-class BaseTalkerFilter implements TalkerFilter {
-  BaseTalkerFilter({
-    @Deprecated('Use keys instead. This feature will be removed in v5.0.0')
-    this.titles = const [],
-    @Deprecated('Use keys instead. This feature will be removed in v5.0.0')
-    this.types = const [],
+class TalkerFilter implements _Filter<TalkerData> {
+  TalkerFilter({
     this.keys = const [],
     this.searchQuery,
   });
-
-  /// List of enabled for filter titles [exception], [error], [verbose]
-  @Deprecated('Use keys instead. This feature will be removed in v5.0.0')
-  final List<String> titles;
-
-  /// List of enabled for filter types - subclasses of [TalkerData]
-  /// Like [TalkerError], [TalkerException], [TalkerLog], etc.
-  @Deprecated('Use keys instead. This feature will be removed in v5.0.0')
-  final List<Type> types;
 
   /// List of enabled for filter keys
   /// This is a new way to filter logs by their keys.
@@ -43,61 +28,16 @@ class BaseTalkerFilter implements TalkerFilter {
 
     if (keys.isNotEmpty) {
       keysMatch = keys.contains(item.key);
-      return searchMatch && keysMatch;
     }
 
-    /// Old filter logic for titles and types
-    /// This is deprecated and will be removed in v5.0.0
-    // ignore: deprecated_member_use_from_same_package
-    return _oldFilterLogic(item);
+    return searchMatch && keysMatch;
   }
 
-  @Deprecated('This method will be removed in v5.0.0')
-  bool _oldFilterLogic(TalkerData item) {
-    if (titles.isEmpty && types.isEmpty && (searchQuery?.isEmpty ?? true)) {
-      return true;
-    } else {
-      var match = false;
-
-      if (searchQuery?.isNotEmpty ?? false) {
-        final fullMsg = item.generateTextMessage();
-        final fullUpperMsg = fullMsg.toUpperCase();
-        final fullLowerMsg = fullMsg.toLowerCase();
-        match = fullUpperMsg.contains(searchQuery!) ||
-            fullLowerMsg.contains(searchQuery!);
-      }
-
-      return (titles.contains(item.title) || titles.isEmpty) &&
-          (_checkTypeMatch(item) || types.isEmpty) &&
-          (match || (searchQuery?.isEmpty ?? true));
-    }
-  }
-
-  @Deprecated('This method will be removed in v5.0.0')
-  bool _checkTypeMatch(TalkerData item) {
-    var match = false;
-    for (final type in types) {
-      if (item.runtimeType == type) {
-        match = true;
-        break;
-      }
-    }
-    return match;
-  }
-
-  BaseTalkerFilter copyWith({
-    @Deprecated('Use keys instead. This feature will be removed in v5.0.0')
-    List<String>? titles,
-    @Deprecated('Use keys instead. This feature will be removed in v5.0.0')
-    List<Type>? types,
+  TalkerFilter copyWith({
     List<String>? keys,
     String? searchQuery,
   }) {
-    return BaseTalkerFilter(
-      // ignore: deprecated_member_use_from_same_package
-      titles: titles ?? this.titles,
-      // ignore: deprecated_member_use_from_same_package
-      types: types ?? this.types,
+    return TalkerFilter(
       keys: keys ?? this.keys,
       searchQuery: searchQuery ?? this.searchQuery,
     );
